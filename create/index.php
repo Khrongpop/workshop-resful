6f07bd76b4f3cfd2cfa1358e1856490cbc972e14
Muang <mr.khrongpop@gmail.com>
2019-06-28 11:53:43 +0700

    add created_at

--- create/index.php
+++ create/index.php
@@ -1,14 +1,14 @@
 <?php
- @include "../db.php";
-  $title = $_POST["title"];
-  $detail = $_POST["detail"];
+@include "../db.php";
+$title = $_POST["title"];
+$detail = $_POST["detail"];
 
-  $sql = "INSERT INTO articles (title, detail) VALUES ( $title,  $detail)";
+$sql = "INSERT INTO articles (title , detail) VALUES ( '$title' , '$detail' )";
 
-	if ($conn->query($sql) === TRUE) {
-		echo "New record created successfully";
-	} else {
-		echo "Error: " . $sql . "<br>" . $conn->error;
-	}
+if ($conn->query($sql) === true) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
 $conn->close();
--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
 @include "db.php";
 
-$sql = "SELECT * FROM articles";
+$sql = "SELECT * FROM articles ORDER BY created_at desc";
 $result = $conn->query($sql);
 
 //Initialize array variable
-  $data = array();
+$datas = array();
 
 //Fetch into associative array
-  while ( $row = $result->fetch_assoc())  {
-	$data[]=$row;
-  }
+while ($row = $result->fetch_assoc()) {
+    $datas[] = $row;
+}
 
 header('Content-Type: application/json');
-echo json_encode($data);
+echo json_encode($datas, JSON_NUMERIC_CHECK);
 
 $conn->close();
